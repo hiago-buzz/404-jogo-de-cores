532c7ead1c5f8d2d184f7026904fd9535c326a9a
hiago santos <foto.hiago@gmail.com>
2019-07-25 16:45:33 -0300

    add classe para girar quadrados

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let quadrados = document.querySelectorAll ("div");
 let resposta = document.querySelector ("h2");
+let reset = document.querySelector("button");
 
-let cores =["red", "blue", "yellow", "Magenta", "aqua", "black", "green"];
+
+let cores =["red", "blue", "yellow"];
 
 function gerarCor(){
 
     let aleatorio = Math.floor (Math.random()* cores.length);
-   
     this.style.backgroundColor = cores[aleatorio];
 
     verificador();
@@ -14,7 +15,6 @@ function gerarCor(){
 }
 
 for (let quadrado of quadrados){
-
     quadrado.onclick = gerarCor;
 
 }
@@ -26,7 +26,26 @@ function verificador (){
     quadrados[2].style.backgroundColor == quadrados[3].style.backgroundColor){
 
         resposta.innerHTML = "você Venceu!!!"
+
+        for (let quadrado of quadrados){
+            quadrado.onclick = null;
+            
+            setInterval (() => {
+                quadrado.classList.add ("girarQuadrado");
+
+           },1000);
+     
         }
 }
 
+}
 
+function resetarJogo(){
+    for(let quadrado of quadrados){
+        quadrado.style.backgroundColor = "unset";
+    }
+
+    resposta.innerHTML = "";
+}
+
+reset.onclick = resetarJogo;
